c419e0b0a026e4bccce5f20bc1b5d48f99d187b0
Misael Reyes <misaelrey90@gmail.com>
2021-11-30 20:50:10 -0600

    relocated images foler and added text animation

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,21 @@
 import React from 'react';
-
+import background from "../images/AdobeStock_190318318.jpeg";
+import { Image } from "react-bootstrap"
+import "../index.css"
 function About(){
     return(
     <>
-    <img class="img-fluid vh-100 vw-100" src="./assets/images/AdobeStock_190318318.jpeg" alt="gradeint background"/>
-    <h1 id="hero1" class="text-center inline-flex position-absolute top-50 start-50 translate-middle">
-        Hello I'm <br/> Misael Reyes
-    </h1>
-    <h1 id="hero2" class="text-center inline-flex position-absolute top-50 start-50 translate-middle">
-        I'm a<br/>Web Developer
-    </h1>
+    <img class="img-fluid w-100 vh-100 " src={ background } alt="gradeint background"/>
+    <div class="overly-img card-img-overlay center-heroku ">
+        <h1 id="hero1" class="line-1 m-auto align-items-center">
+            Hello I'm Misael Reyes
+        </h1>
+        <br/>
+        <h1 id="hero2" class="line-2 m-auto align-items-center">
+            I'm a Web Developer
+        </h1>
+    </div>
+    
     <article id="About-me" class=" flex-wrap row align-items-center justify-content-center container-fluid">
         <img id="portrait" class="rounded-circle col-md-3 py-3"
             src="https://clinicforspecialchildren.org/wp-content/uploads/2016/08/avatar-placeholder.gif"
