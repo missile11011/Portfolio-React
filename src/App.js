d08be48b2a65d92addb874f7f41795ec01880641
Misael Reyes <misaelrey90@gmail.com>
2021-11-30 21:26:09 -0600

    redesigned navbar

--- src/App.js
+++ src/App.js
@@ -5,14 +5,12 @@ import About from "./components/About";
 import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import GridImage from "./components/GridImage";
 
 function App() {
   return (
   <>
     <Header></Header>
     <About></About>
-    <GridImage></GridImage>
     <Portfolio></Portfolio>
     <Contact></Contact>
     <Footer></Footer>
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,36 +3,36 @@ import React from 'react';
 function Header(){
     return(
     <>
-    <nav class="navbar bg-transparent navbar-expand-lg position-absolute top-0 container-fluid">
+    <nav class="navbar bg-dark navbar-expand-lg position-absolute top-0 container-fluid">
         <button class="navbar-toggler btn btn-dark p-2" type="button" data-bs-toggle="collapse"
             data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
             aria-label="Toggle navigation">
             Menu
         </button>
         <div class="collapse navbar-collapse">
-            <ul class="navbar-nav ms-auto">
+            <ul class="navbar-nav ms-auto m-auto py-3">
                 <li class="">
-                    <a class=" p-3 mb-2 text-white text-decoration-none" href="#">Home</a>
+                    <a class="p-5 mb-2 text-white text-decoration-none" href="#">Home</a>
                 </li>
                 <li class="">
-                    <a class=" p-3 mb-2 text-white text-decoration-none" href="#Portfolio">Work</a>
+                    <a class="p-5 mb-2 text-white text-decoration-none" href="#Portfolio">Work</a>
                 </li>
                 <li>
-                    <a class="p-3 mb-2 text-white text-decoration-none" href="#About-me">About</a>
+                    <a class="p-5 mb-2 text-white text-decoration-none" href="#About-me">About</a>
                 </li>
                 <li>
-                    <a class=" p-3 mb-2 text-white text-decoration-none" href="#Contact">Contact</a>
+                    <a class="p-5 mb-2 text-white text-decoration-none" href="#Contact">Contact</a>
                 </li>
             </ul>
         </div>
     </nav>
     <div class="collapse navbar-collapse fs-1 bg-light text-center" id="navbarSupportedContent">
-        <ul class="navbar-nav">
+        <ul class="navbar-nav ">
             <li class="">
                 <a class=" p-5 mb-2 text-dark text-decoration-none" href="#">Home</a>
             </li>
             <li class="">
-                <a class=" p-5 mb-2 text-dark text-decoration-none" href="#Portfolio">Work</a>
+                <a class=" p-5 mb-2 text-dark text-decoration-none" href="#Portfolio">Works</a>
             </li>
             <li>
                 <a class=" p-5 mb-2 text-dark text-decoration-none" href="#About-me">About</a>
