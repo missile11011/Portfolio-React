554279c27aa012b655b99e8d07ef88e77efc7baf
Misael Reyes <misaelrey90@gmail.com>
2021-12-17 23:26:59 -0600

    resume pdf added portfolio hover reveal bug fixed

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,22 +2,29 @@ import React from 'react';
 
 function Contact(){
     return(
-        <section id="Contact" class="container-fluid">
+    <section id="Contact" class="container-fluid">
+        <div class=" text-center">
+            <h2 class="text-center my-5">My Resume</h2>
+        <a class="btn btn-primary btn-lg px-5 text-white" href={process.env.PUBLIC_URL + '/GeneralResume.PDF'}>Resume PDF</a>
+        </div>
+        
         <h2 class="text-center my-5">
             Get in touch
         </h2>
-        <ul class="row mb-5">
+        <ul class="row mb-5 list-unstyled">
             <li class='col text-center '>
                 <h4>Phone Number</h4>
-                <p>(123)456-7890</p>
+                <p>(972)946-5331</p>
             </li>
             <li class='col text-center'>
                 <h4>Email</h4>
-                <p>misaelrey90@gmail.com</p>
+                <a class="text-decoration-none text-black"
+                    href="mailto:misaelrey90@gmail.com">misaelrey90@gmail.com</a>
+
             </li>
             <li class="col text-center">
                 <h4>Github</h4>
-                <a class="text-decoration-none"
+                <a class="text-decoration-none text-black"
                     href="https://github.com/missile11011/portfolio">github.com/missile11011</a>
             </li>
         </ul>
--- src/components/GridImage.js
+++ src/components/GridImage.js
@@ -2,12 +2,17 @@ import React, { useState, useEffect, useRef } from "react";
 import {Container, Row, Col, Image} from "react-bootstrap";
 
 const mouseOverHandler = (e) => {
-    const border = e.target.parentElement.lastElementChild;
-    border.className="card-img-overlay bg-light text-center"
+    const card = e.target.parentElement.lastElementChild;
+    card.className="card-img-overlay bg-light text-center"
+    for (let i = 0; i < card.childNodes.length; i++) {
+        card.childNodes[i].className="card-text p-1"
+    }
 }
 const mouseOutHandler = (e) =>{
-    const border = e.target
-    border.className="hidden"
+    const card = e.target
+    if (card.element="div"){
+        card.className="hidden"
+    }
 }
 const GridImage = () => {
 
@@ -18,56 +23,56 @@ const GridImage = () => {
         <Row class="">
             <Col lg={4} md={6} sm={12} className="p-0   card" >
                 <Image className="img-fluid card-img"  onMouseOver={mouseOverHandler} src={process.env.PUBLIC_URL + '/images/WeatherDash.PNG'} />
-                <div className="hidden" onMouseLeave={mouseOutHandler} >
-                    <h5 class="card-title">Weather Dashboard</h5>
-                    <p class="card-text">Weather dashboard web application that displays current forecast and daily forecast</p>
-                    <a class="card-link" href="https://github.com/missile11011/WeatherDash" target="_blank">GitHub</a>
-                    <a class="card-link" href="https://missile11011.github.io/WeatherDash/" target="_blank">Project</a>
+                <div class="hidden " onMouseLeave={mouseOutHandler} >
+                    <h5 class="card-text">Weather Dashboard</h5>
+                    <p class="card-text">Weather dashboard web application that displays current forecast and daily forecast.</p>
+                    <a class="card-text p-1" href="https://github.com/missile11011/WeatherDash" target="_blank">GitHub</a>
+                    <a class="card-text p-1" href="https://missile11011.github.io/WeatherDash/" target="_blank">Project</a>
                 </div>
             </Col>
             <Col lg={4} md={6} sm={12} className="p-0  card">
                 <Image onMouseOver={mouseOverHandler}  className="img-fluid card-img" src= {process.env.PUBLIC_URL + '/images/ReduxStore.png'} />
-                <div className="hidden" onMouseLeave={mouseOutHandler} >
-                    <h5 class="card-title">Book Search</h5>
-                    <p class="card-text">Searchs for books and save them for late </p>
-                    <a class="card-link" href="https://github.com/missile11011/ReduxStore" target="_blank">GitHub</a>
-                    <a class="card-link" href="https://redux-shop-51615.herokuapp.com/" target="_blank">Project</a>
+                <div class="hidden" onMouseLeave={mouseOutHandler} >
+                    <h5 class="card-text p-1">Redux Shop</h5>
+                    <p class="card-text p-1">An e-commerce site using react and redux.</p>
+                    <a class="card-text p-1" href="https://github.com/missile11011/ReduxStore" target="_blank">GitHub</a>
+                    <a class="card-text p-1" href="https://redux-shop-51615.herokuapp.com/" target="_blank">Project</a>
                 </div>
             </Col>
             <Col lg={4} md={6} sm={12} className="p-0  card">
                 <Image className="img-fluid card-img" onMouseOver={mouseOverHandler} src= {process.env.PUBLIC_URL + '/images/Booksearch.png'} />
-                <div className="hidden" onMouseLeave={mouseOutHandler} >
-                    <h5 class="card-title">Book Search</h5>
-                    <p class="card-text">Searchs for books and save them for late </p>
-                    <a class="card-link" href="https://github.com/missile11011/book_search" target="_blank">GitHub</a>
-                    <a class="card-link" href="https://book-search-25563.herokuapp.com/" target="_blank">Project</a>
+                <div class="hidden" onMouseLeave={mouseOutHandler} >
+                    <h5 class="card-text p-1">Book Search</h5>
+                    <p class="card-text p-1">Searchs for books and save them for later.</p>
+                    <a class="card-text p-1" href="https://github.com/missile11011/book_search" target="_blank">GitHub</a>
+                    <a class="card-text p-1" href="https://book-search-25563.herokuapp.com/" target="_blank">Project</a>
                 </div>
             </Col>
             <Col lg={4} md={6} sm={12} className="p-0 card">
                 <Image className="img-fluid card-img" onMouseOver={mouseOverHandler} src= {process.env.PUBLIC_URL + '/images/Password.png'} />
-                <div className="hidden" onMouseLeave={mouseOutHandler} >
-                    <h5 class="card-title">Password Generator</h5>
-                    <p class="card-text">This web application generates a password for you. It creates a random password with certain parameters.</p>
-                    <a class="card-link" href="https://github.com/missile11011/PasswordGenerator">GitHub</a>
-                    <a class="card-link" href="https://missile11011.github.io/PasswordGenerator/">Project</a>
+                <div class="hidden" onMouseLeave={mouseOutHandler} >
+                    <h5 class="card-text p-1">Password Generator</h5>
+                    <p class="card-text p-1">This application creates a random password with certain parameters.</p>
+                    <a class="card-text p-1" href="https://github.com/missile11011/PasswordGenerator" target="_blank">GitHub</a>
+                    <a class="card-text p-1" href="https://missile11011.github.io/PasswordGenerator/" target="_blank">Project</a>
                 </div>
             </Col>                      
             <Col lg={4} md={6} sm={12} className="p-0 card">
                 <Image className="img-fluid card-img" onMouseOver={mouseOverHandler} src= {process.env.PUBLIC_URL + '/images/Budget.png'} />
-                <div className="hidden" onMouseLeave={mouseOutHandler} >
-                    <h5 class="card-title">Budget Tracker</h5>
+                <div class="hidden" onMouseLeave={mouseOutHandler} >
+                    <h5 class="card-text p-1">Budget Tracker</h5>
                     <p class="card-text">This tracks your budget, and helps helps you keep track of your spending habits.</p>
-                    <a class="card-link" href="https://github.com/missile11011/BudgetTracker">GitHub</a>
-                    <a class="card-link" href="https://budget-84554.herokuapp.com/">Project</a>
+                    <a class="card-text p-1" href="https://github.com/missile11011/BudgetTracker" target="_blank">GitHub</a>
+                    <a class="card-text p-1" href="https://budget-84554.herokuapp.com/" target="_blank">Project</a>
                 </div>
             </Col>
             <Col lg={4} md={6} sm={12} className="p-0 card">
                 <Image className="img-fluid card-img" onMouseOver={mouseOverHandler} src= {process.env.PUBLIC_URL + '/images/Password.png'} />
                 <div className="hidden" onMouseLeave={mouseOutHandler} >
-                    <h5 class="card-title">Password Generator</h5>
-                    <p class="card-text">This web application generates a password for you. It creates a random password with certain parameters.</p>
-                    <a class="card-link" href="https://github.com/missile11011/PasswordGenerator">GitHub</a>
-                    <a class="card-link" href="https://missile11011.github.io/PasswordGenerator/">Project</a>
+                    <h5 className="card-text p-1">Password Generator</h5>
+                    <p className="card-text">This web application generates a password for you. It creates a random password with certain parameters.</p>
+                    <a className="card-text p-1" href="https://github.com/missile11011/PasswordGenerator" target="_blank">GitHub</a>
+                    <a className="card-text p-1" href="https://missile11011.github.io/PasswordGenerator/" target="_blank">Project</a>
                 </div>
             </Col>
         </Row>
